2ab8c6fdae00bfb334f907bb89e14170c5e77a83
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-12-14 03:07:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from utils.runs import *
-import sys
-import matplotlib
 
-if 'ipykernel' in sys.modules:
-    # IPython 환경이라면 inline 백엔드 사용
-    matplotlib.use('module://ipykernel.pylab.backend_inline')
-    
+import argparse
+from utils.runs import *    
+
+parser = argparse.ArgumentParser(description="Run tandem solar cell efficiency analysis.")
+parser.add_argument('--Tcell', type=float, help="Cell temperature in Kelvin", required=True)
+parser.add_argument('--Egap', type=float, help="Energy gap for single PSC in eV", required=True)
+parser.add_argument('--Egap_min', type=float, help="Minimum bandgap for tandem PSC in eV", required=True)
+parser.add_argument('--Egap_max', type=float, help="Maximum bandgap for tandem PSC in eV", required=True)
+parser.add_argument('--bottom_bandgap', type=float, help="Bottom cell's bandgap for tandem in eV", required=True)
+parser.add_argument('--top_bandgap', type=float, help="Top cell's bandgap for tandem in eV", required=True)
+
+args = parser.parse_args()
+
 # Spectrum Comparison
 spectrumS, spectrumE = analyze_spectrum()
 
 # Analyze Single PSC
-Tcell = float(input("Enter cell temperature (in Kelvin): "))
-Egap = float(input("Enter energy gap for single PSC (in eV): "))
-calc_single_PSC(Egap, Tcell, spectrumS, spectrumE)
+calc_single_PSC(args.Egap, args.Tcell, spectrumS, spectrumE)
 
 # Calculate Tandem PSC
-Egap_min = float(input("Enter minimum bandgap for tandem PSC (in eV): "))
-Egap_max = float(input("Enter maximum bandgap for tandem PSC (in eV): "))
+calc_tandem_PSC(args.Tcell, spectrumS, spectrumE, args.Egap_min, args.Egap_max, args.bottom_bandgap, args.top_bandgap)
 
-bottom_bandgap = float(input("Enter the bottom cell's bandgap you want for tandem (in eV): "))
-top_bandgap = float(input("Enter the top cell's bandgap you want for tandem (in eV): "))
-
-calc_tandem_PSC(Tcell, spectrumS, spectrumE, Egap_min, Egap_max, bottom_bandgap, top_bandgap)
+#Usage : %run main.py --Tcell=300 --Egap=1.1 --Egap_min=0.5 --Egap_max=2.5 --bottom_bandgap=1.2 --top_bandgap=1.8
